ce8b36c697eba46fb85107ab2895b45aa43bcc88
IcyPebble <100799513+IcyPebble@users.noreply.github.com>
2024-01-21 15:10:48 +0100

    Fixed title animation issue

--- static/index.js
+++ static/index.js
@@ -1,29 +1,6 @@
-// Title
-let screen = document.getElementById("screen");
-fetch("static/RouteFinder.svg").then((res) => res.text()).then(
-    (svgString) => {
-        let doc = new DOMParser().parseFromString(svgString, "image/svg+xml");
-        let svg = doc.getElementsByTagName("svg")[0];
-
-        svg.id = "title";
-
-        screen.insertBefore(svg, screen.firstChild);
-
-        Array.from(
-            document.getElementsByClassName("AnimationOffset")
-        ).forEach(
-            (s) => {
-                s.attributes.dur.value = "0.6s";
-            }
-        );
-    }
-)
-
-
-// Input
-fileInput = document.getElementById("fileInput");
-fileInputBtn = document.getElementById("fileInputBtn");
-startBtn = document.getElementById("startBtn");
+let fileInput = document.getElementById("fileInput");
+let fileInputBtn = document.getElementById("fileInputBtn");
+let startBtn = document.getElementById("startBtn");
 
 if (fileInput.files.length > 0) {
     fileInput.value = '';
